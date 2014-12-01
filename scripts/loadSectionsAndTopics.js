ceb254a37aa05cffb220d7d9042ea01b1500c88f
MalinaD <malina.demirova@gmail.com>
2014-12-01 12:27:52 +0200

    Added show topic categories

--- scripts/loadSectionsAndTopics.js
+++ scripts/loadSectionsAndTopics.js
@@ -53,8 +53,13 @@ $(function () {
 
 	    			$.each(topics, function (index, topic) {
 	    				if (topic.section.objectId == sectionId) {	    					
-	    					var nextTopic = $('<div class="topic">'+topic.name+'</div>');	    					
-	    					topicsDiv.append(nextTopic);							
+	    				    var nextTopic = $('<div class="topic"> </div>');
+	    				    var topicLink = $("<a href=\"#\">").text(topic.name + '  ');
+	    				    $(topicLink).data('topic', topic);
+	    				    topicLink.append(nextTopic);
+	    				    $(topicLink).click(topicClicked);
+	    				    topicsDiv.append(topicLink);
+
 	    				}	    				
 	    			});
 	    		},
@@ -72,7 +77,37 @@ $(function () {
 
 	$('#categories').on('click', 'li a', function (e) {
 		loadTopics($(this).data('sectionId'));				             
-	});	
+	});
+
+	function topicClicked() {
+	    var topic = $(this).data('topic');
+	    $("#posts").hide();
+	    $("#topics").hide();
+	    $("#posts h2").text(topic.name);
+	    $("#posts h2").data('country', topic);
+	    var topicId = topic.objectId;
+	    $.ajax({
+	        method: "GET",
+	        headers: headers,
+	        url: 'https://api.parse.com/1/classes/Post?where={"topic":{"__type":"Pointer","className":"Topic","objectId":"' + topicId + '"}}',
+	        success: postsLoaded,
+	        error: function (err) {
+	            errorMessage(err, 'Error occured when loading posts');
+	        }
+	    });
+	}
+
+	function postsLoaded(data) {
+	    $("#posts ul").html('');
+	    for (var a in data.results) {
+	        var post = data.results[a];
+	        var postItem = $('<p>');
+	        postItem.text(post.text + ' ');
+	        postItem.appendTo($("#posts div"));
+	    }
+	    $('#posts').show();
+	}
+
 
 	loadSections();
 });
