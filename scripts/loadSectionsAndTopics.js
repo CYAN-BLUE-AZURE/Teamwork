b443afb53344279e3f7fbacbffac2f185dbd7d7e
anichalakova <anichalakova@users.noreply.github.com>
2014-12-07 01:53:32 +0200

    Finished advertisement function

--- scripts/loadSectionsAndTopics.js
+++ scripts/loadSectionsAndTopics.js
@@ -93,255 +93,262 @@ $(function () {
 					errorMessage(err, 'Error occured when loading topics');
 				}
 			});
-}
-
-function loadAdvertisement() {	
-	$.ajax({
-		url: urlAdvertisements,
-		type: 'GET',
-		headers: HEADERS,
-		success: function (data) {        			    			
-			var adverts = data.results;
-			var randomIndex = Math.floor((Math.random() * adverts.length)); 
-			var currentAdvertLink = $('<a class="addvertLink" href="#"></a>');
-			currentAdvertLink.appendTo(CONTENT);
-			var currentAdvertImage = $('<img>').addClass('advert').attr('src', adverts[randomIndex].file.url).attr('height', '300');				
-			currentAdvertImage.appendTo(currentAdvertLink);
-			console.log(adverts[randomIndex].file.url);			       			
-		},
-		error: function (err) {
-			errorMessage(err.responseText, "Error occurred while loading advertisement.");
 		}
-	});
-}
 
-
-
-function getDateTimeForPrint(inputDate){
-	var hours = inputDate.getHours();
-	var minutes = inputDate.getMinutes();
-	var seconds = inputDate.getSeconds();
-	var date = inputDate.getDay();
-	var month = inputDate.getMonth();
-	var year = inputDate.getFullYear();
-
-	var result =
-	date + '.' + month + '.' + year + ' ' + 
-	hours + ':' + minutes + ':' + seconds;
-
-	return result;
-}
-
-function drawPostsFromTopic(topicId,topicName){
-	$.ajax({
-		url:urlPosts,
-		type:"GET",
-		headers:HEADERS,
-		data: {
-			'where':JSON.stringify({
-				topic:{
-					__type:"Pointer",
-					className: "Topic",
-					objectId: topicId
+		function loadAdvertisement() {	
+			$.ajax({
+				url: urlAdvertisements,
+				type: 'GET',
+				headers: HEADERS,
+				success: function (data) {        			    			
+					var adverts = data.results;
+					var randomIndex = Math.floor((Math.random() * adverts.length)); 
+					var currentAdvertLink = $('<a class="advertLink" href="'+ adverts[randomIndex].href +'" target="_blank"></a>');
+					var currentAdvertImage = $('<img>').addClass('advert').attr('src', adverts[randomIndex].file.url).attr('height', '300');				
+					currentAdvertImage.appendTo(currentAdvertLink);
+					currentAdvertLink.appendTo(CONTENT);
+					var hideAdvertButton = $('<button id="hideAdvertBtn"></button>').on('click', function() {				
+						$('.advertLink').remove();
+						$(this).remove();
+					});
+					hideAdvertButton.appendTo(CONTENT);				       			
+				},
+				error: function (err) {
+					errorMessage(err.responseText, "Error occurred while loading advertisement.");
 				}
-			}),
-			'include':'fromUser',
-			'order': '-createdAt'
-		},
-		success: function (data){
-			CONTENT.html('');
-			var posts = data.results;
-
-			var postContainer = $('<div class="post-container">');
-			postContainer.appendTo(CONTENT);
-
-			var headerTopic = $('<h1 class="topic-header">');
-			headerTopic.text(topicName);
-			headerTopic.appendTo(postContainer);
-
-			$.each(posts, function(index, post) {
-				var nextPost = $('<div class="post">');
-				nextPost.text(post.text);
-				nextPost.appendTo(postContainer);
-
-				var postDetails = $('<p class="details"></p>');
-				postDetails.appendTo(nextPost);
-
-				var fromUser = $('<span>From: </span>');
-
-				var spanFromUser = $('<span class="from-user"></span>');
-				spanFromUser.text(post.fromUser.username);
-				spanFromUser.appendTo(fromUser);
-
-				fromUser.appendTo(postDetails);
-				var createdAt = $('<span class="created-at">');
-
-				var createdDate = getDateTimeForPrint(new Date(post.createdAt));
-
-				createdAt.text(' CreatedAt: ' + createdDate);
-				createdAt.appendTo(postDetails);
-
-				var addPostButtonContainer = $('<div class="add-post-container">');
-				addPostButtonContainer.appendTo(postDetails);
-
-				var addPostBtn = $('<a href="#" class="add-post">Add post</a>');
-				addPostBtn.on('click',function(){
-					addPost(topicId);
-				});
-				addPostBtn.appendTo(addPostButtonContainer);
 			});
-		},
-		error: function (){
-
 		}
-	})
-}
 
-function addPost(topicId){
-	//TODO:
-	console.log(topicId);
-}
 
-		// function getSectionsById (sectionId) {        	
-		// 	return sections.filter(function (sections) {
-		// 		return sectionId == sections.objectId
-		// 	});        	
-		// }
 
-		// $('#categories').on('click', 'li a', function (e) {
-		// 	loadTopics($(this).data('sectionId'));				             
-		// });
 
-    //ADD FUNCTIONS
- //    $('#addCategoryBtn').on('click', function () {
- //    	var $categoryName = $('#addCategory').val();
- //    	if (/^\s*$/.test($categoryName)) {
+		function getDateTimeForPrint(inputDate){
+			var hours = inputDate.getHours();
+			var minutes = inputDate.getMinutes();
+			var seconds = inputDate.getSeconds();
+			var date = inputDate.getDay();
+			var month = inputDate.getMonth();
+			var year = inputDate.getFullYear();
 
-	//        //TODO validation
+			var result =
+			date + '.' + month + '.' + year + ' ' + 
+			hours + ':' + minutes + ':' + seconds;
 
-	//        return;
-	//    }
-	//    addCategory($categoryName);
-	//    $('#addCategory').val('');
-	// });
+			return result;
+		}
 
-    // function addCategory(category) {
-    // 	var categoryName = formatString(category);
+		function drawPostsFromTopic(topicId,topicName){
+			$.ajax({
+				url:urlPosts,
+				type:"GET",
+				headers:HEADERS,
+				data: {
+					'where':JSON.stringify({
+						topic:{
+							__type:"Pointer",
+							className: "Topic",
+							objectId: topicId
+						}
+					}),
+					'include':'fromUser',
+					'order': '-createdAt'
+				},
+				success: function (data){
+					CONTENT.html('');
+					var posts = data.results;
 
-    // 	$.ajax({
-    // 		method: "POST",
-    // 		headers: HEADERS,
-    // 		url: urlSections,
-    // 		data: JSON.stringify({
-    // 			"name": categoryName
-    // 		}),
-    // 		contentType: "application/json",
-    // 		success: [sectionCleaned(),loadSections()],
-    // 		error: function (err) {
-    // 			errorMessage(err, 'Error occured while loading sections');
-    // 		}
-    // 	});
-    // }
+					var postContainer = $('<div class="post-container">');
+					postContainer.appendTo(CONTENT);
 
-    // $('#addTopicBtn').on('click', function () {
-    // 	var $topicName = $('#addTopic').val();
-    // 	if (/^\s*$/.test($topicName)) {
-	   //      //addEmptyItemError();
-    //         //TODO validation
-    //         return;
-    //     }
-    //     addTopic($topicName);
-    //     $('#addTopic').val('');
-    // });
+					var headerTopic = $('<h1 class="topic-header">');
+					headerTopic.text(topicName);
+					headerTopic.appendTo(postContainer);
 
-    // function addTopic(topic) {
-    // 	var topicName = formatString(topic);
-    // 	var section = $('#topics h2').data('section');
+					$.each(posts, function(index, post) {
+						var nextPost = $('<div class="post">');
+						nextPost.text(post.text);
+						nextPost.appendTo(postContainer);
 
-    // 	$.ajax({
-    // 		method: "POST",
-    // 		headers: HEADERS,
-    // 		url: urlTopics,
-    // 		data: JSON.stringify({
-    // 			"name": topicName,
-    // 			'section': {
-    // 				"__type": "Pointer",
-    // 				"className": "Section",
-    // 				"objectId": section.objectId
-    // 			}
-    // 		}),
-    // 		contentType: "application/json",
-    // 		success: [topicCleaned(), loadTopics()],
-    // 		error: function (err) {
-    // 			errorMessage(err, 'Error occured while loading topics');
-    // 		}
-    // 	});
-    // }
+						var postDetails = $('<p class="details"></p>');
+						postDetails.appendTo(nextPost);
 
-    // function sectionClicked() {
-    // 	var topic = $(this).data('section');
-    // 	$("#posts").hide();
-    // 	$("#topicsMain").show();
+						var fromUser = $('<span>From: </span>');
 
-    // }
+						var spanFromUser = $('<span class="from-user"></span>');
+						spanFromUser.text(post.fromUser.username);
+						spanFromUser.appendTo(fromUser);
 
-    //Functions for cleaning and refreshing
-    // function sectionCleaned() {
-    // 	$("#sections").empty();
-    // }
-    // function topicCleaned() {
-    // 	$("#topics").empty();
-    // }
+						fromUser.appendTo(postDetails);
+						var createdAt = $('<span class="created-at">');
 
-    // function topicClicked() {
-    // 	var topic = $(this).data('topic');
-    // 	$("#posts").hide();
-    // 	$("#topicsMain").hide();
-    // 	$("#posts h2").text(topic.name);
-    // 	$("#posts h2").data('country', topic);
-    // 	var topicId = topic.objectId;
-    // 	$.ajax({
-    // 		method: "GET",
-    // 		headers: HEADERS,
-    // 		url: 'https://api.parse.com/1/classes/Post?where={"topic":{"__type":"Pointer","className":"Topic","objectId":"' + topicId + '"}}',
-    // 		success: postsLoaded,
-    // 		error: function (err) {
-    // 			errorMessage(err, 'Error occured while loading posts');
-    // 		}
-    // 	});
-    // }
+						var createdDate = getDateTimeForPrint(new Date(post.createdAt));
 
-    // function postsLoaded(data) {
-    // 	$("#posts div").html('');
-    // 	for (var a in data.results) {
-    // 		var post = data.results[a];
+						createdAt.text(' CreatedAt: ' + createdDate);
+						createdAt.appendTo(postDetails);
 
-	   //      //showing the username
+						var addPostButtonContainer = $('<div class="add-post-container">');
+						addPostButtonContainer.appendTo(postDetails);
 
-    //         //showing the text of the post
-    //         var postItem = $('<p>');
-    //         postItem.text(post.text + ' ');
+						var addPostBtn = $('<a href="#" class="add-post">Add post</a>');
+						addPostBtn.on('click',function(){
+							addPost(topicId);
+						});
+						addPostBtn.appendTo(addPostButtonContainer);
+					});
+				},
+				error: function (){
 
-	   //      //showing the date of update
-	   //      var postDate = $('<p>');
-	   //      var currentDate = post.createdAt;
-	   //      var date = $.format.prettyDate(currentDate);
-	   //      postDate.text(date + ' ');
+				}
+			})
+		}
 
-	   //      postItem.appendTo($("#posts div"));
-	   //      postDate.appendTo($("#posts div"));
-	   //  }
+		function addPost(topicId){
+			//TODO:
+			console.log(topicId);
+		}
 
-    //     //date, text, username
-    //     $('#posts').show();
-    // }
+					// function getSectionsById (sectionId) {        	
+					// 	return sections.filter(function (sections) {
+					// 		return sectionId == sections.objectId
+					// 	});        	
+					// }
 
-    // function formatString(string) {
-    // 	var trimmed = string.trim();
-    // 	return trimmed.charAt(0).toUpperCase() + string.slice(1);
-    // }
+					// $('#categories').on('click', 'li a', function (e) {
+					// 	loadTopics($(this).data('sectionId'));				             
+					// });
 
-    loadSections();
-	//loadAdvertisement();
+				//ADD FUNCTIONS
+			 //    $('#addCategoryBtn').on('click', function () {
+			 //    	var $categoryName = $('#addCategory').val();
+			 //    	if (/^\s*$/.test($categoryName)) {
+
+				//        //TODO validation
+
+				//        return;
+				//    }
+				//    addCategory($categoryName);
+				//    $('#addCategory').val('');
+				// });
+
+				// function addCategory(category) {
+				// 	var categoryName = formatString(category);
+
+				// 	$.ajax({
+				// 		method: "POST",
+				// 		headers: HEADERS,
+				// 		url: urlSections,
+				// 		data: JSON.stringify({
+				// 			"name": categoryName
+				// 		}),
+				// 		contentType: "application/json",
+				// 		success: [sectionCleaned(),loadSections()],
+				// 		error: function (err) {
+				// 			errorMessage(err, 'Error occured while loading sections');
+				// 		}
+				// 	});
+				// }
+
+				// $('#addTopicBtn').on('click', function () {
+				// 	var $topicName = $('#addTopic').val();
+				// 	if (/^\s*$/.test($topicName)) {
+				   //      //addEmptyItemError();
+				//         //TODO validation
+				//         return;
+				//     }
+				//     addTopic($topicName);
+				//     $('#addTopic').val('');
+				// });
+						
+				// function addTopic(topic) {
+				// 	var topicName = formatString(topic);
+				// 	var section = $('#topics h2').data('section');
+
+				// 	$.ajax({
+				// 		method: "POST",
+				// 		headers: HEADERS,
+				// 		url: urlTopics,
+				// 		data: JSON.stringify({
+				// 			"name": topicName,
+				// 			'section': {
+				// 				"__type": "Pointer",
+				// 				"className": "Section",
+				// 				"objectId": section.objectId
+				// 			}
+				// 		}),
+				// 		contentType: "application/json",
+				// 		success: [topicCleaned(), loadTopics()],
+				// 		error: function (err) {
+				// 			errorMessage(err, 'Error occured while loading topics');
+				// 		}
+				// 	});
+				// }
+
+				// function sectionClicked() {
+				// 	var topic = $(this).data('section');
+				// 	$("#posts").hide();
+				// 	$("#topicsMain").show();
+
+				// }
+
+				//Functions for cleaning and refreshing
+				// function sectionCleaned() {
+				// 	$("#sections").empty();
+				// }
+				// function topicCleaned() {
+				// 	$("#topics").empty();
+				// }
+
+				// function topicClicked() {
+				// 	var topic = $(this).data('topic');
+				// 	$("#posts").hide();
+				// 	$("#topicsMain").hide();
+				// 	$("#posts h2").text(topic.name);
+				// 	$("#posts h2").data('country', topic);
+				// 	var topicId = topic.objectId;
+				// 	$.ajax({
+				// 		method: "GET",
+				// 		headers: HEADERS,
+				// 		url: 'https://api.parse.com/1/classes/Post?where={"topic":{"__type":"Pointer","className":"Topic","objectId":"' + topicId + '"}}',
+				// 		success: postsLoaded,
+				// 		error: function (err) {
+				// 			errorMessage(err, 'Error occured while loading posts');
+				// 		}
+				// 	});
+				// }
+
+				// function postsLoaded(data) {
+				// 	$("#posts div").html('');
+				// 	for (var a in data.results) {
+				// 		var post = data.results[a];
+
+				   //      //showing the username
+
+				//         //showing the text of the post
+				//         var postItem = $('<p>');
+				//         postItem.text(post.text + ' ');
+
+				   //      //showing the date of update
+				   //      var postDate = $('<p>');
+				   //      var currentDate = post.createdAt;
+				   //      var date = $.format.prettyDate(currentDate);
+				   //      postDate.text(date + ' ');
+
+				   //      postItem.appendTo($("#posts div"));
+				   //      postDate.appendTo($("#posts div"));
+				   //  }
+
+				//     //date, text, username
+				//     $('#posts').show();
+				// }
+
+				// function formatString(string) {
+				// 	var trimmed = string.trim();
+				// 	return trimmed.charAt(0).toUpperCase() + string.slice(1);
+				// }
+
+		loadSections();
+		loadAdvertisement();
+
+	});
 });
-});
+
